d291fcd97dfb08fcb9664631b4796795487a8e36
southislander99 <melwest000@gmail.com>
2022-05-17 19:24:57 +0800

    Added edit and delete Student functions
    edit a student profile
    delete a student profile

--- groupProjectBackend/students/serializers.py
+++ groupProjectBackend/students/serializers.py
@@ -21,3 +21,26 @@ class StudentsSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Students.objects.create(**validated_data)
+
+
+class StudentsDetailSerializer(StudentsSerializer):
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name',instance.name)
+        instance.contact_email = validated_data.get('contact_email',instance.contact_email)
+        instance.contact_phone = validated_data.get('contact_phone',instance.contact_phone)
+        instance.biography = validated_data.get('biography',instance.biography)
+        instance.location = validated_data.get('location',instance.location)
+        instance.demographic_gender = validated_data.get('demographic_gender',instance.demographic_gender)
+        instance.demographic_nationality = validated_data.get('demographic_nationality',instance.demographic_nationality)
+        instance.social_linkedin = validated_data.get('social_linkedin',instance.social_linkedin)
+        instance.social_github = validated_data.get('social_github',instance.social_github)
+        instance.employment_company = validated_data.get('emplayment_company',instance.employment_company)
+        instance.employment_position = validated_data.get('employment_position',instance.employment_position)
+        instance.employment_industry = validated_data.get('employment_industry',instance.employment_industry)
+        instance.employment_salary = validated_data.get('employment_salary',instance.employment_salary)
+        instance.program_attendence = validated_data.get('program_attendence',instance.program_attendence)
+        instance.coding_languages = validated_data.get('coding_languages',instance.coding_languages)
+        instance.save()
+        return instance
+
--- groupProjectBackend/students/views.py
+++ groupProjectBackend/students/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from django.http import Http404
 from rest_framework import status
 from .models import Students
-from .serializers import StudentsSerializer
+from .serializers import StudentsSerializer, StudentsDetailSerializer
 
+#Students listing function. Get a list of students and post to create new students
 class StudentsList(APIView):
 
     def get(self, request):
@@ -25,6 +26,9 @@ class StudentsList(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+# Students detail view. View details of a program on a single page ie. students/1/
+# put function to edit student detail
+# del function to delete a single student
 class StudentsDetail(APIView):
 
     def get_object(self, pk):
@@ -37,5 +41,34 @@ class StudentsDetail(APIView):
         students = self.get_object(pk)
         serializer = StudentsSerializer(students)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        students = self.get_object(pk)
+        data = request.data
+        serializer = StudentsDetailSerializer(
+            instance = students,
+            data = data,
+            partial = True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.errors,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        students = self.get_object(pk)
+        students.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+        
+
 
 
