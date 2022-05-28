2016b39a77b7a445526d10096932b48e57ea178d
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-05-28 10:00:24 +0800

    Users
    All users urls are working and functional

--- groupProjectBackend/groupProjectBackend/urls.py
+++ groupProjectBackend/groupProjectBackend/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reports.urls')),
     path('', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
 
