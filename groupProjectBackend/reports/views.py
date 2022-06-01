66a4719eb33c012893ad71b04738e1bbb0b95201
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-06-01 20:25:08 +0800

    Update views.py

--- groupProjectBackend/reports/views.py
+++ groupProjectBackend/reports/views.py
@@ -65,12 +65,12 @@ class ReportDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-                )
-        return Response(
                 serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
         )
 
 
